f351ab40dd050e8b3e666e1ad691469ae8548eb3
Maksym <maxthebest1906@gmail.com>
2021-02-16 16:14:22 +0200

    task ready

---
+++ src/js/change-theme.js
@@ -0,0 +1,27 @@
+import refs from './refs.js';
+
+const { toggle, body } = refs;
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+const { LIGHT, DARK } = Theme;
+toggle.addEventListener('change', toggleThemeSeting);
+
+function toggleThemeSeting() {
+    if (toggle.checked) {
+      body.setAttribute('class', DARK);
+      localStorage.setItem('theme', DARK);
+    } else {
+      body.setAttribute('class', LIGHT);
+      localStorage.setItem('theme', LIGHT);
+    }
+}
+
+  if (localStorage.theme === DARK) {
+    toggle.checked = true;
+    body.setAttribute('class', DARK);
+  } else {
+    toggle.checked = false;
+    body.setAttribute('class', LIGHT);
+  }
